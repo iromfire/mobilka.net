281a7aab1b5a0732b5a59085a2a8fef49fa9b4ed
artur <green.neh@gmail.com>
2023-06-09 22:03:59 +0300

    визуеальные изменения

--- src/app/user/main-page/main-page.component.ts
+++ src/app/user/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../shared/services/product.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Product } from '../../shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-main-page',
@@ -10,13 +11,17 @@ import { map } from 'rxjs/operators';
 })
 export class MainPageComponent implements OnInit {
   productName!: string;
+
   products = this.productServ.products.pipe(
     map((products) => products.filter((p) => p.quantityStock! > 0))
   );
+
   isLoaded: Observable<boolean> = this.productServ.isLoaded;
+  favouriteItems: Product[] = [];
   constructor(private productServ: ProductService) {}
 
   ngOnInit(): void {
     this.productServ.getAll();
+    this.favouriteItems = this.productServ.getFavouriteItems();
   }
 }
